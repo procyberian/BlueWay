9524d308258486cdd0a9c4be159a36e42804c911
Mert Gör <mertgor@masscollabs.xyz>
2023-09-06 17:24:30 +0300

    some logical fix for email_password.py example

--- email_password.py
+++ email_password.py
@@ -21,21 +21,20 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 email_list = []
 
 def email():
-    email = input('email is:')
-    if email == 'quit':
-        print('wrong email')
-    else:
-        email_list.append(email)
+    while True:
+        email = input('email is:')
+        if email == 'quit':
+            break
+        else:
+            email_list.append(email)
         
 def password():
-    password = input('password is:')
-    if password == 'quit':
-        print('wrong email')
-    else:
-        email_list.append(password)        
+    while True:
+        password = input('password is:')
+        if password == 'quit':
+            break
+        else:
+            email_list.append(password)        
 
-def display_email_password(email, password):
-    avatar = email + " " +password
-    print("my avatar is: ", avatar)
-    
-display_email_password('myemail@myemail.com', 'myemail')
+email()
+password()
